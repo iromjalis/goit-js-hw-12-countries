52161117c4f78f74bc4c01086bec09f61160d0c1
Irina Romjalis <iromjalis@gmail.com>
2021-04-02 18:33:19 +0200

    error pnotify

--- src/index.js
+++ src/index.js
@@ -1,33 +1,48 @@
 import './styles.css';
-import templates from './template/template.hbs';
-import {debounce} from 'throttle-debounce';
 
-const refs= {
-  searchFrom: document.querySelector('.input'),
-  countryContainer : document.querySelector('.countryContainer')
-  }
+import {  defaultModules,error} from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyDesktop from '@pnotify/desktop/dist/PNotifyDesktop';
+
+import "@pnotify/core/dist/PNotify.css" 
+// import "@pnotify/mobile/dist/PNotifyMobile.css" ;
+import "@pnotify/desktop/dist/PNotifyDesktop" ;
+import '@pnotify/core/dist/Material.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+
+defaultModules.set(PNotifyDesktop, {});
+
+error('Too many matches found. Please enter a more specific query!');
+// error('Error!');
+
+//&=================
+// const refs= {
+//   searchFrom: document.querySelector('.input'),
+//   countryContainer : document.querySelector('.countryContainer')
+//   }
   
-refs.searchFrom.addEventListener('input', (e) => {
- e.preventDefault()
+// refs.searchFrom.addEventListener('input', (e) => {
+//  e.preventDefault()
 
- refs.countryContainer.innerHTML = ''
+//  refs.countryContainer.innerHTML = ''
 
- const inputValue = e.currentTarget.value;
- if(inputValue !== ' ' || inputValue.value.length >= 2){
-fetchCountry (inputValue).then(data=> updateMarkup(data))}
-})
+//  const inputValue = e.currentTarget.value;
+//  if(inputValue !== ' ' || inputValue.value.length >= 2){
+// fetchCountry (inputValue).then(data=> updateMarkup(data))}
+// })
 
-//~запрос
-function fetchCountry (inputValue){
-  return fetch(`https://restcountries.eu/rest/v2/name/${inputValue}`)
-.then(data=>data.json())
-.catch(error=>console.log('No country founded, sorry'))
-}
+// //~запрос
+// function fetchCountry (inputValue){
+//   return fetch(`https://restcountries.eu/rest/v2/name/${inputValue}`)
+// .then(data=>data.json())
+// .catch(error=>console.log('No country founded, sorry'))
+// }
 
-//~отриcовка карточек на странице
-function updateMarkup (data){
-  const markup = templates(data)
-  if(data!== ''&& data!== null){
-  refs.countryContainer.insertAdjacentHTML('afterbegin', markup)}
-}
+// //~отриcовка карточек на странице
+// function updateMarkup (data){
+//   const markup = templates(data)
+//   if(data!== ''&& data!== null){
+//   refs.countryContainer.insertAdjacentHTML('afterbegin', markup)}
+// }
 
+//counrties.forEach(country=> document.quearySelector('.container').innerHTML += `<li>${country.name}</li>`)
