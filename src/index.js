3f5b1f4523d9ed7d965192619b5108d13cc9df01
Irina Romjalis <iromjalis@gmail.com>
2021-04-02 18:53:48 +0200

    error window +  if на input

--- src/index.js
+++ src/index.js
@@ -1,48 +1,74 @@
 import './styles.css';
 
-import {  defaultModules,error} from '@pnotify/core/dist/PNotify.js';
+import templates from './template/template.hbs'
+import {  defaultModules,error} from '@pnotify/core';
 import * as PNotifyDesktop from '@pnotify/desktop/dist/PNotifyDesktop';
 
 import "@pnotify/core/dist/PNotify.css" 
-// import "@pnotify/mobile/dist/PNotifyMobile.css" ;
 import "@pnotify/desktop/dist/PNotifyDesktop" ;
-import '@pnotify/core/dist/Material.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 
 defaultModules.set(PNotifyDesktop, {});
+// error({
+//   text: 'Too many matches found. Please enter a more specific query!',
+//   styling:'brighttheme',
 
-error('Too many matches found. Please enter a more specific query!');
-// error('Error!');
+// });
+
 
 //&=================
-// const refs= {
-//   searchFrom: document.querySelector('.input'),
-//   countryContainer : document.querySelector('.countryContainer')
-//   }
+const refs= {
+  searchFrom: document.querySelector('.input'),
+  countryContainer : document.querySelector('.countryContainer')
+  }
   
-// refs.searchFrom.addEventListener('input', (e) => {
-//  e.preventDefault()
+refs.searchFrom.addEventListener('input', (e) => {
+ e.preventDefault()
 
-//  refs.countryContainer.innerHTML = ''
+ refs.countryContainer.innerHTML = ''
 
-//  const inputValue = e.currentTarget.value;
-//  if(inputValue !== ' ' || inputValue.value.length >= 2){
-// fetchCountry (inputValue).then(data=> updateMarkup(data))}
-// })
+ const inputValue = e.currentTarget.value;
+ if(inputValue !== ' ' || inputValue.value.length >= 2){
+fetchCountry (inputValue).then(data=> updateMarkup(data))}
+})
 
-// //~запрос
-// function fetchCountry (inputValue){
-//   return fetch(`https://restcountries.eu/rest/v2/name/${inputValue}`)
-// .then(data=>data.json())
-// .catch(error=>console.log('No country founded, sorry'))
-// }
+//~запрос
+function fetchCountry (inputValue){
+  return fetch(`https://restcountries.eu/rest/v2/name/${inputValue}`)
+.then(data=>data.json())
+.catch(text=> { error({
+     text: 'Too many matches found. Please enter a more specific query!',
+     styling:'brighttheme',
+   })}
+  )
+}
 
-// //~отриcовка карточек на странице
-// function updateMarkup (data){
-//   const markup = templates(data)
-//   if(data!== ''&& data!== null){
-//   refs.countryContainer.insertAdjacentHTML('afterbegin', markup)}
-// }
+//~отриcовка карточек на странице
+function updateMarkup (data){
+  const markup = templates(data)
+  console.log(data.length);
+  if(data.length === undefined){
+    error({
+      text: ` Please enter a more specific query!`,
+      styling:'brighttheme',
+      delay: 1000,
+    });
+    return
+  }
+  
+  if(data.length >= 5  ){
+    error({
+      title: `Too many matches found.`,
+      text: `We found ${data.length} countries. Please enter a more specific query!`,
+      styling:'brighttheme',
+      delay: 1000,
 
-//counrties.forEach(country=> document.quearySelector('.container').innerHTML += `<li>${country.name}</li>`)
+    });
+    return data.forEach(country=>refs.countryContainer.innerHTML += `<li>${country.name}</li>`);
+  }
+
+  if(data!== '' && data !== null && data !== undefined){
+  refs.countryContainer.insertAdjacentHTML('afterbegin', markup)}
+}
+
